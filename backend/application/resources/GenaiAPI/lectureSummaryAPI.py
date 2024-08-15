4eac7fabd409387e875e13b11ba4b0e26370ac1e
Prahladh Chinappa Nochilur <128896585+PrahladhCN1@users.noreply.github.com>
2024-08-15 19:42:33 +0530

    Update lectureSummaryAPI.py

--- backend/application/resources/GenaiAPI/lectureSummaryAPI.py
+++ backend/application/resources/GenaiAPI/lectureSummaryAPI.py
@@ -2,14 +2,27 @@ from application.models.model import db, CourseContent
 from flask import jsonify, request
 from flask_restful import Resource
 from .genAI import generate_summary
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
 
+def YouTubeTranscript(video_id):
+    '''
+        Fetches the transcript of the video with the given video_id
+        "video_id is the video id of the youtube video example: 'dQw4w9WgXcQ' 
+        in a link like https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    '''
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    formatter = TextFormatter()
+    return formatter.format_transcript(transcript)
 
 class LectureSummaryAPI(Resource):
     def get(self, weeknumber, id):
         content = CourseContent.query.filter_by(week=weeknumber, id=id).first()
         if not content:
             return {"message": "No transcript found for this week"}, 404
-        return {'summary' : generate_summary(content.transcript)}, 200
+        video_id=content.link
+        transcript=YouTubeTranscript(video_id)
+        return {'summary' : generate_summary(transcript)}, 200
     
     def post(self):
         pass
