14a4deba9d68999e1352715ac5da4812a9b996e1
ShubhamHarkare <shubham.21910407@viit.ac.in>
2024-08-15 14:17:13 +0530

    updated the youtube links

--- backend/application/resources/GenaiAPI/genAI.py
+++ backend/application/resources/GenaiAPI/genAI.py
@@ -12,6 +12,8 @@ history = ''
 tokens = [
     "LL-7u9HFmV7Zyu9sCyDE7NpY7hjFfHt6p7h6Po9pLAi9d4VgkjwTTL9bnN51cKsDwJ7",
     "LL-Qm2wzWhTyYYAJV2kWgGhcbCLIVZBCOzSDuib1smXSsouoANM1YtgSfp3v9IlyQPW",
+    "LA-dbfcbb80618f4c7ea1c27f4e20af79fd4af3c30849f94657aef232d0197082b5",
+    "LA-51215ccf1b454a63a7d70253a0ac65b4c55758b2e56042e2a590def77849fe83"
     # Add more tokens if required
 ]
 
